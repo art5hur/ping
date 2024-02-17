eb80778b1b07a06ae731daad7b6a384e1a16d77a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-16 22:45:03 -0300

    first commit

---
+++ ping/src/test/java/com/github/art5hur/ping/PingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.art5hur.ping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
